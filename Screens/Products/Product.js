b764b30861b38872ce4cbf87b8ab88d3e85bf7eb
vijay <vijayskumarsuresh@gmail.com>
2023-01-12 17:03:05 +0530

    Get the Details from FakeStore API

--- Screens/Products/Product.js
+++ Screens/Products/Product.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import ProductList from "./ProductList";
 import { Products } from "../../Data/Products";
 import { StyleSheet, View } from "react-native";
 import Filter from "../../Components/Filter";
 
 const Product = () => {
+
+  const [products, setProducts] = useState(null);
+
+  useLayoutEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+    };
+    getData();
+  });
+
   return (
     <View style={styles.screen}>
       <View style={styles.filterWrapper}>
-        <Filter items={Products} />
-        <ProductList items={Products} />
+        { products && <Filter items={products} /> }
+        { products && <ProductList items={products} /> }
       </View>
     </View>
   );
