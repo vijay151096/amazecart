568214ff77b7dc046487d81a871452815b63f86d
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-01-25 12:08:47 +0530

    added image in assets folder

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,10 +1,11 @@
 import React, {useContext, useEffect} from 'react';
-import {Text} from 'react-native';
+import {Text, Image} from 'react-native';
 import LogoutIcon from '../Components/Core/LogoutIcon';
 
 const Profile = () => {
   return (
     <>
+      <Image source={require('../assets/images/sample.jpeg')} />
       <Text>Profile</Text>
       <LogoutIcon />
     </>
