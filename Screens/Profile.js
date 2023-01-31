5a9555ade6c7c50ec8e521f3605274b9182ccaf3
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-01-31 15:58:45 +0530

    added web view

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,14 +1,14 @@
 import React, {useContext, useEffect} from 'react';
 import {Text, Image} from 'react-native';
 import LogoutIcon from '../Components/Core/LogoutIcon';
+import { WebView } from 'react-native-webview';
 
 const Profile = () => {
   return (
-    <>
-      <Image source={require('../assets/images/sample.jpeg')} />
-      <Text>Profile</Text>
-      <LogoutIcon />
-    </>
+    <WebView
+      source={{uri: 'http://localhost:3000/'}}
+      style={{marginTop: 20}}
+    />
   );
 };
 
