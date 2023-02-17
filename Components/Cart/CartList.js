200d7ab8274380efb392902b4ece024a21ddbb62
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-02-17 16:30:10 +0530

    added animation to lists

--- Components/Cart/CartList.js
+++ Components/Cart/CartList.js
@@ -1,9 +1,14 @@
 import React, {useContext} from 'react';
 import {FlatList} from 'react-native';
+import Animated, {FadeIn, FadeOut, Layout} from 'react-native-reanimated';
 import CartItem from './CartItem';
 
 const renderItems = item => {
-  return <CartItem item={item} />;
+  return (
+    <Animated.View entering={FadeIn} exiting={FadeOut} layout={Layout}>
+      <CartItem item={item} />
+    </Animated.View>
+  );
 };
 
 const CartList = ({cartProducts}) => {
--- Components/Favorite/FavoriteList.js
+++ Components/Favorite/FavoriteList.js
@@ -1,12 +1,18 @@
 import React, {useContext} from 'react';
 import {FlatList} from 'react-native';
+import Animated, {FadeIn, FadeOut, Layout} from 'react-native-reanimated';
 import {FavoriteContext} from '../../Store/FavoriteContextProvider';
 import FavoriteItem from './FavoriteItem';
 
 function FavoriteList({bottomSheetHandler}) {
   const {favoriteProducts} = useContext(FavoriteContext);
-  const renderItem = ({item}) => (
-    <FavoriteItem item={item} bottomSheetHandler={bottomSheetHandler} />
+  const renderItem = ({item, index}) => (
+    <Animated.View
+      entering={FadeIn.delay(index * 250)}
+      exiting={FadeOut}
+      layout={Layout}>
+      <FavoriteItem item={item} bottomSheetHandler={bottomSheetHandler} />
+    </Animated.View>
   );
   return (
     <FlatList
