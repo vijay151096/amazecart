6a4705cd2b63aee233c83dc274f715c4902dc873
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2023-02-17 12:24:42 +0530

    removed unwanted imports in navigation

--- Navigation/DrawerNavigator.js
+++ Navigation/DrawerNavigator.js
@@ -1,31 +1,8 @@
 import React, {useContext} from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ProductDashBoard from '../Screens/ProductDashBoard';
-import ProductDetails from '../Screens/ProductDetails';
-import Cart from '../Screens/Cart';
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-} from '@react-navigation/native';
 import Title from '../Components/Core/Title';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import DeleteCart from '../Components/Cart/DeleteCart';
-import BoldTitle from '../Components/Core/BoldTitle';
-import {AuthContext} from '../Store/AuthContextProvider';
-import Login from '../Screens/Login';
-import Signup from '../Screens/Signup';
-import Profile from '../Screens/Profile';
-import LogoutIcon from '../Components/Core/LogoutIcon';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import Favorites from '../Screens/Favorites';
-import {ActivityIndicator, View} from 'react-native';
-import GithubWebView from '../Screens/GithubWebView';
 import Donate from '../Screens/Donate';
 import {ThemeContext} from '../Store/ThemeContextProvider';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import ShoppingIcon from '../Components/Core/ShoppingIcon';
-import FavoriteIcons from '../Components/Core/FavoriteIcons';
 import BottomTabNavigator from './BottomTabNavigator';
 
 const Drawer = createDrawerNavigator();
--- Navigation/MainNavigation.js
+++ Navigation/MainNavigation.js
@@ -1,5 +1,4 @@
 import React, {useContext} from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   DarkTheme,
   DefaultTheme,
